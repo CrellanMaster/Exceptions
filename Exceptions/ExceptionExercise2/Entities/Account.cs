e8d3c1a6abcc815b07b48f5e1a96175ff6f38de0
CrellanMaster <devsandrounity@gmail.com>
2022-07-12 22:21:36 -0300

    ExceptionExercise2 Solved

---
+++ Exceptions/ExceptionExercise2/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using ExceptionExercise2.Entities.Exceptions;
+
+namespace ExceptionExercise2.Entities
+{
+    internal class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            if (balance < 50)
+            {
+                throw new DomainException("The minimum account amount must be $50");
+            }
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+            }
+            if (Balance == 0 || Balance < 0 || amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ Exceptions/ExceptionExercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExceptionExercise2.Entities.Exceptions;
+using ExceptionExercise2.Entities;
+using System.Globalization;
+
+namespace ExceptionExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, name, balance, withdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(withdraw);
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
